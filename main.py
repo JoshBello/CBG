6d3cd4e930653461803ac92dc24da743c878c0d3
Josh <josh.bello@vodafone.com>
2021-09-07 12:32:44 +0100

    New Font

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import textwrap
 
 parts_dict = {}
 parts = ['adverbs', 'verbs', 'adjectives', 'nouns']
+font_path = '/Library/Fonts/Assistant-VariableFont_wght.ttf'
+weight = [b'ExtraLight', b'Light', b'Regular', b'SemiBold', b'Bold', b'ExtraBold']
+# font_path = '/Library/Fonts/Arial.ttf'
 
 def dict_generator(parts):
 
@@ -30,16 +33,20 @@ def pick_random_words(parts_dict, parts):
 dict_generator(parts)
 sentence_str = pick_random_words(parts_dict, parts)
 
+sentence_str = 'You can using You You can using You You can using You You can using You You can using You You can using You'
 para = textwrap.wrap(sentence_str, width=25)
 
-base_heights = {2 : 200,
-                3 : 175,
-                4 : 145}
+base_heights = {1 : 220,
+                2 : 190,
+                3 : 155,
+                4 : 125,
+                5 : 90}
 
 width, height = 500, 500
 img = Image.new('RGB', (width, height), (0, 0, 0, 0))
 draw = ImageDraw.Draw(img)
-font = ImageFont.truetype('/Library/Fonts/Arial.ttf', 34)
+font = ImageFont.truetype(font_path, 38)
+font.set_variation_by_name('Regular')
 
 base_height = base_heights[len(para)]
 padding = 18
